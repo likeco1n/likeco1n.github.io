145b979434d666bedd93033f84c1716e5ce766e2
David Adler <dalberto.adler@gmail.com>
2017-07-23 19:22:38 +0200

    update

--- js/agency.js
+++ js/agency.js
@@ -39,6 +39,9 @@
             $('#particleground-background')
                 .width($('header').width())
                 .height($('header').height());
+            $('#particleground-background canvas')
+                .width($('header').width())
+                .height($('header').height());
         })
     }
     $(window).resize(resizeParticleGround)
@@ -51,10 +54,33 @@
         );
       });
     $('#big-timer').countdown('2017/07/30', function(event) {
-        $(this).text(
-          event.strftime('%D days %H.%M.%S')
-        );
+        // $(this).text(
+        //   event.strftime('%D days %H.%M.%S')
+        // );
+        console.info('got here');
+        var units = ['days', 'hours', 'minutes', 'seconds'];
+        for (var i = 0; i < units.length; i ++) {
+            var unit = units[i];
+            // console.info('event.offset[unit]', event.offset[unit]);
+            $(this).find('.' + unit + ' > .number').text(event.offset[unit])
+        }
       });
 
 
 })(jQuery); // End of use strict
+
+// offset: {
+//        seconds     : '{int} Seconds left for the next minute',
+//        minutes     : '{int} Minutes left for the next hour',
+//        hours       : '{int} Hours left until next day',
+//        days        : '{int} Days left until next week',
+//        daysToWeek  : '{int} Days left until next week',
+//        daysToMonth : '{int} Days left until next month',
+//        weeks       : '{int} Weeks left until the final date',
+//        weeksToMonth: '{int} Weeks left until the next month',
+//        months      : '{int} Months left until final date',
+//        years       : '{int} Years left until final date',
+//        totalDays   : '{int} Total amount of days left until final date',
+//        totalHours  : '{int} Total amount of hours left until final date',
+//        totalMinutes: '{int} Total amount of minutes left until final date',
+//        totalSeconds
